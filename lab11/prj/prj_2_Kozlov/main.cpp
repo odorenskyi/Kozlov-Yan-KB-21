74f2a878a7d9d61a23a70f22af7f5ede05d3c981
Yan Kozlov <kozlov.yan1@gmail.com>
2022-05-06 22:58:13 +0300

    Added prj_2_Kozlov

---
+++ lab11/prj/prj_2_Kozlov/main.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <iomanip>
+
+#include "ModulesKozlov.h"
+#include "ModulesZadorozhny.h"
+#include "ModulesIsachenkov.h"
+
+using namespace std;
+
+int main()
+{
+    system("chcp 1251");
+    vector<entry> database;
+    string filename = "database.dat";
+    loadDatabase(database, filename);
+    do {
+        system("cls");
+        int option = 0;
+        cout << setfill('=') << setw(48) << "|ВІДДІЛ КАДРІВ|" << setw(32) << "" << endl;
+        cout << setfill(' ') << "| " << setw(77) << left << " Це програмне забезпечення "
+                "створене для роботи з особовими картками" << "|" << endl;
+        cout << "| " << setw(77) << left << "працівників ф. П-2. Нижче наведено список "
+                "опцій, доступних користувачу." << "|" << endl;
+        cout << "| " << setw(77) << "" << "|" << endl;
+        cout << "| " << setw(77) << "1. Вивести всю базу даних на екран або у текстовий файл." << "|" << endl;
+        cout << "| " << setw(77) << "2. Додати новий запис до бази даних." << "|" << endl;
+        cout << "| " << setw(77) << "3. Пошук запису за прізвищем." << "|" << endl;
+        cout << "| " << setw(77) << "4. Вилучити запис з бази даних." << "|" << endl;
+        cout << "| " << setw(77) << "5. Завершити роботу програми." << "|" << endl;
+        cout << setfill('=') << setw(80) << "" << endl << setfill(' ');
+        cout << "Введіть номер операції, яку ви бажаєте виконати: " << endl << ">>> ";
+        if (!(cin >> option)) {
+            cin.clear();
+            cin.ignore();
+        }
+        cout << resetiosflags(ios::left);
+        string surname = "";
+        int id = 0;
+        switch (option) {
+            case 1:
+                showDatabase(database);
+                break;
+            case 2:
+                AddPersonalCard(database);
+                break;
+            case 3:
+                cout << "Введіть прізвище працівника, чию картку ви шукаєте: " << endl << ">>> ";
+                cin >> surname;
+                searchPersonalCard(database, surname);
+                break;
+            case 4:
+                cout << "Введіть ідентифікаційний номер картки, яку ви хочете вилучити: " << endl << ">>> ";
+                while (!(cin >> id)) {
+                    cin.clear();
+                    cin.ignore();
+                }
+                RemovePersonalCard(database, id);
+                break;
+            case 5:
+                saveDatabase(database, filename);
+                return 0;
+            default:
+                continue;
+        }
+        system("pause");
+    } while(true);
+}
